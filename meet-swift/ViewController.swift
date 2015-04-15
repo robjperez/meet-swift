c01485730d1d3183ec0c01c9698d9fe7f14c9808
rpc <rpc@rpcs-MacBook-Pro.local>
2015-04-15 17:08:34 +0200

    Initial Commit

---
+++ meet-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meet-swift
+//
+//  Created by rpc on 15/04/15.
+//  Copyright (c) 2015 tokbox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
